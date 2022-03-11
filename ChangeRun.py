4cd3956eb7fa5fe05fd0357bd2320a20dcdb6dc0
Cathal McLoughlin <cathal.mc-loughlin@ucdconnect.ie>
2022-03-11 14:44:36 +0000

    Update ChangeRun.py

--- ChangeRun.py
+++ ChangeRun.py
@@ -6,20 +6,21 @@ runtime=sys.argv[2]
 cores=sys.argv[3]
 net=sys.argv[4]
 
-
+assert (cores<9)
 assert net in ["Small_world","Scale_free","Spatial","Regular","SBlock"]
 
 fin = open("mRun.sh", "rt")
 fout = open("Run.sh", "wt", newline='')
 
 for line in fin:
-    if line.strip()=="#SBATCH -n 10":
-        new_core = "#SBATCH -n "+str(cores)
+    if line.strip()=="SLURM_NTASKS=6":
+        new_core = "#SLURM_NTASKS="+str(cores)
         fout.write(new_core+"\n")
     
     elif line.strip()=="#SBATCH -t TIME":
         new_time = "#SBATCH -t "+str(runtime)+":00:00"
         fout.write(new_time+"\n")
+        
     elif line.strip()=="model_dir=\"${HOME}/storage/ClusterRuns\"":
         new_dir="model_dir=\"${HOME}/storage/"+str(newloc) +"\""
         fout.write(new_dir+"\n")
