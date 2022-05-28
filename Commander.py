f5eda723980bf534d2f6e0d8bb52f511411c710e
Cathal McLoughlin <cathal.mc-loughlin@ucdconnect.ie>
2022-05-28 11:17:46 +0100

    Update Commander.py

--- Commander.py
+++ Commander.py
@@ -27,7 +27,7 @@ SimControllerMaster.params["n"] = 500
 #SimControllerMaster.params["k"] = 20
 SimControllerMaster.params["h"] = 0.02
 
-nitit=40 
+nitit=100
 ps = {"ImprovedSpatial": np.linspace(0.5,10,nitit),#np.linspace(0,8.5,1000),
     "Small_world":np.geomspace(1e-3,1,nitit),#np.geomspace(1e-4,1,1000) ,
         "Scale_free": np.linspace(1e-4,4,nitit), 
@@ -47,7 +47,7 @@ net = sys.argv[3]
 torun2 = ps[net]
 torun =[]
 for v in torun2:
-    for r in [0,0.2,0.4,0.6,0.8,1.0]:
+    for r in np.linspace(0,1,11):
         torun.append( {"net":net,"p":v,"r":r}) 
 
 
